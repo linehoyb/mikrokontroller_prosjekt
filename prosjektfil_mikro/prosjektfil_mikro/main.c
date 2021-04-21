254b4ff97cfc8f349e0ced7a78385f855624b011
linehoyb <63612725+linehoyb@users.noreply.github.com>
2021-04-21 11:39:27 +0200

    Lagt til prosjektfil

---
+++ prosjektfil_mikro/prosjektfil_mikro/main.c
@@ -0,0 +1,18 @@
+/*
+ * prosjektfil_mikro.c
+ *
+ * Created: 21.04.2021 11:38:20
+ * Author : lineh
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
