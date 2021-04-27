47816935776bd36bfb444c9ec1a924e1c332d951
sanbjring <74248406+sanbjring@users.noreply.github.com>
2021-04-27 10:21:24 +0200

    Button added

--- prosjekt_timer/main.c
+++ prosjekt_timer/main.c
@@ -38,6 +38,25 @@ void Stop()
 }
 
 
+void button_interupt()
+{
+	  PORTD |= (1 << PD2);  /* initialize pullup resistor on our input pin */
+	  DDRB = 0xff;                           /* set up all LEDs for output */
+
+	  // ------ Event loop ------ //
+	  while (1) {
+		  if (bit_is_clear(PIND, PD2)) {            /* look for button press */
+			  /* equivalent to if ((PIND & (1 << PD2)) == 0 ){ */
+				  PORTB = 0b00111100;                                   /* pressed */
+			  }
+			  else {                                              /* not pressed */
+				  PORTB = 0b11000011;
+			  }
+		 }                                                  /* End event loop */	 	
+}
+
+
+
 int main(void)
 {
 	DDRB = (1<<DDB0);
